724c9a01470d1cc4534e42fbb3452f10c605d7d1
KesterDaniel <kesterdan17@gmail.com>
2023-02-01 11:54:56 +0100

    created a list of words from alphabet dict based on user input

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import pandas
 
 alphabet_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (index, row) in alphabet_df.iterrows()}
+
+user_word = input("Enter a word: ").upper()
+user_word_list = [alphabet_dict[letter] for letter in user_word]
+print(user_word_list)
