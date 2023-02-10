ee7b08141b979d335af5a58f7bf5d9bb7db3a114
KesterDaniel <kesterdan17@gmail.com>
2023-02-10 09:47:14 +0100

    catching KeyErrors

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import pandas
 alphabet_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (index, row) in alphabet_df.iterrows()}
 
-user_word = input("Enter a word: ").upper()
-user_word_list = [alphabet_dict[letter] for letter in user_word]
-print(user_word_list)
+def generate_phonetic():
+    try:
+        user_word = input("Enter a word: ").upper()
+        user_word_list = [alphabet_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(user_word_list)
+
+generate_phonetic()
